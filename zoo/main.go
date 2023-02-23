0c1f76e267e5667b23d277ac94bb720da3517867
Yusuke Yasuo <yasuo.y@dely.jp>
2023-02-24 00:42:14 +0900

    zoo

---
+++ zoo/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"go_study/zoo/animals"
+)
+
+func main() {
+	fmt.Println(animals.ElephantFeed())
+	fmt.Println(animals.MonkeyFeed())
+	fmt.Println(animals.RabbitFeed())
+}
