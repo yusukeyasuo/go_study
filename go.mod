0c34d3b6939554a8b95389fed2c292f412dadc02
Yusuke Yasuo <yasuo.y@dely.jp>
2023-04-15 22:32:45 +0900

    third party library

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module go_study
 
 go 1.20
+
+require (
+	github.com/markcheno/go-quote v0.0.0-20220624214117-555891babbf1 // indirect
+	github.com/markcheno/go-talib v0.0.0-20190307022042-cd53a9264d70 // indirect
+)
--- main.go
+++ main.go
@@ -3,19 +3,13 @@ package main
 import (
 	"fmt"
 
-	"go_study/mylib"
-	"go_study/mylib/under"
+	"github.com/markcheno/go-quote"
+	"github.com/markcheno/go-talib"
 )
 
 func main() {
-	s := []int{1, 2, 3, 4, 5}
-	fmt.Println(mylib.Average(s))
-
-	mylib.Say()
-	under.Hello()
-	person := mylib.Person{Name: "Mike", Age: 20}
-	fmt.Println(person)
-
-	fmt.Println(mylib.Public)
-	// fmt.Println(mylib.private)
+	spy, _ := quote.NewQuoteFromYahoo("spy", "2016-01-01", "2016-04-01", quote.Daily, true)
+	fmt.Print(spy.CSV())
+	rsi2 := talib.Rsi(spy.Close, 2)
+	fmt.Println(rsi2)
 }
